221fe06d941a992a443c2e647af46bab2e2e4284
alex16541 <aleks.16541@yandex.ru>
2024-06-04 12:18:49 +0300

    Настроить докер

--- vite.config.ts
+++ vite.config.ts
@@ -9,4 +9,12 @@ export default defineConfig({
     resolve: {
         alias: [{ find: '@', replacement: `${path.resolve(__dirname, 'src')}/` }],
     },
+    server: {
+        watch: {
+            usePolling: true,
+        },
+        host: true,
+        strictPort: true,
+        port: 5000,
+    },
 });
